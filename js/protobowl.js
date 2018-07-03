7eea4091aa819a86bf37aa4d62370113deaf379d
shrey150 <shrey150@yahoo.com>
2018-07-03 11:32:28 -0400

    Auto-trim answer lines and display more user info

--- js/protobowl.js
+++ js/protobowl.js
@@ -1,3 +1,7 @@
+console.log("Bot> Hello");
+console.log("Bot> I'm listening for questions to start now");
+console.log("Bot> If you want me to stop, type in 'botStop()'");
+
 const node = document.getElementById("history");
 
 const config = {
@@ -6,6 +10,7 @@ const config = {
 	subtree: false
 }
 
+// listens for when the question starts and starts the bot
 let observer = new MutationObserver((mutationsList) => {
 
 	for (let mutation of mutationsList) {
@@ -19,20 +24,24 @@ let observer = new MutationObserver((mutationsList) => {
 
 });
 
+// synchronous wrapper to make sure bot doesn't crash anything
 function startGuess() {
 	guess().then(complete => {
 		if (!complete) setTimeout(startGuess, 50);
 	});
 }
 
+// starts the listener
 observer.observe(node, config);
 
 function guess() {
 
 	return new Promise((resolve, reject) => {
 
+		// gets all words of the question displayed on screen
 		let words = $(".active .well").children().not(".unread");
 
+		// prevents the bot from hitching
 		if (words.length < 3) {
 
 			console.log("Bot> Too early to buzz...");
@@ -43,6 +52,8 @@ function guess() {
 
 		let question = words.text();
 
+		// config settings for the QuizDB API
+		// TODO: allow customization here
 		let args = {
 
 			"query"			: encodeURI(question),
@@ -54,17 +65,23 @@ function guess() {
 
 		}
 		
+		// quick PHP wrapper to bypass Cross-Origin block in Chrome
 		$.get("http://162.243.36.7/projects/protobowl/api.php", args)
 		.done(function(data) {
 
 			let response = JSON.parse(data);
 
+			// if bot is 100% sure, BUZZ
 			if (response.data.num_tossups_found == 1) {
 
 				console.log("Bot> Found answer!");
 
+				// replaces in this order: brackets, quotes, parentheses
+				let answer = response.data.tossups[0].answer.replace(/ *\[[^\]]*]/, "").replace(/"(.*?(\s)*?)*?"/, "").replace(/ *\[[^)]*\ */g, "");
+
+				// TODO: buzzing in still doesn't work correctly
 				$(".buzzbtn").click();
-				$(".guess_input").val(response.data.tossups[0].answer);
+				$(".guess_input").val(answer);
 				$(".guess_input").trigger( jQuery.Event("keydown", { which: 13, keyCode: 13 }));
 				$(".guess-form").submit();
 
@@ -80,5 +97,12 @@ function guess() {
 		})
 
 	});
+
+}
+
+function stopBot() {
+
+	console.log("Bot> Bye");
+	observer.disconnect();
 
 }
